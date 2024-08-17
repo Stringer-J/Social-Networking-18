3f812924ab27314735f4cc2ddf1095efbb8d5556
Josh Stringer <joshstringer@live.com>
2024-08-17 13:18:51 -0500

    thoughts model done

--- models/Thought.js
+++ models/Thought.js
@@ -2,8 +2,19 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const thoughtSchema = new Schema({
-
+    thoughtText: { type: String, required: true, maxlength: [280, 'Cannot be longer than 280 characters']},
+    createdAt: { type: Date, default: Date.now, get: formatDate },
+    username: { type: String, required: true },
+    reactions: [],
+}, {
+    toJSON: { getters: true },
+    toObject: { getters: true }
 });
+
+function formatDate(date) {
+    if (!date) return null;
+    return date.toISOString().split('T')[0];
+}
 
 const Thought = mongoose.model('Thought', thoughtSchema);
 
