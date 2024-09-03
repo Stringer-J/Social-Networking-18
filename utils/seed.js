9a815e15f7c8530e71ab727b401b9dd9b631194c
Josh Stringer <joshstringer@live.com>
2024-09-03 11:05:32 -0500

    created thought seed, need to figure out how to link them to a user

--- utils/seed.js
+++ utils/seed.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const User = require('../models/User');
+const Thought = require('../models/Thought');
 
 const users = [
     { username: 'Josh', email: 'joshstringer@live.com' },
@@ -12,6 +13,25 @@ const users = [
     { username: 'Bob', email: 'icanpaint@whatever.com' }
 ];
 
+const thoughts = [
+    { thoughtText: `Maybe I actually don't think about anything...`,
+        username: 'Josh', createdAt: new Date() },
+    { thoughtText: `I say I'm from Vermont but I'm from Arizona`,
+        username: 'Mariah', createdAt: new Date() },
+    { thoughtText: `With arms wide open`,
+        username: 'Scott', createdAt: new Date() },
+    { thoughtText: `But this isn't the living room`,
+        username: 'Riley', createdAt: new Date() },
+    { thoughtText: `I'm gonna eat popcorn with a spoon`,
+        username: 'Alice', createdAt: new Date() },
+    { thoughtText: `Just chillin with the Ninja Turtles`,
+        username: 'April', createdAt: new Date() },
+    { thoughtText: `I wonder where all the lightning is?`,
+        username: 'Odin', createdAt: new Date() },
+    { thoughtText: `I stopped painting a few years ago`,
+        username: 'Bob', createdAt: new Date() }
+]
+
 const MONGO_URI = 'mongodb://localhost:27017/social_db';
 
 const seedDB = async () => {
@@ -22,9 +42,12 @@ const seedDB = async () => {
         });
 
         await User.deleteMany({});
+        await Thought.deleteMany({});
 
         await User.insertMany(users);
-        console.log('Seed worked!');
+        console.log('User seed worked!');
+        await Thought.insertMany(thoughts);
+        console.log('Thought seed worked!');
         
     } catch (error) {
         console.error('Seed failed...', error);
