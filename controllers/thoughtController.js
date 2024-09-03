bb4f8a165424f0a4b892db35bcc3b663727f45dd
Josh Stringer <joshstringer@live.com>
2024-09-03 10:43:20 -0500

    thought.find() works

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -3,7 +3,12 @@ const Thought = require('../models/Thought');
 const ThoughtController = {
     
     getAll: async (req, res) => {
-        res.send('this is thought - get all');
+        try {
+            const thoughts = await Thought.find();
+            res.status(200).json(thoughts);
+        } catch (error) {
+            res.status(500).json({ message: 'Error fetching thoughts', error });
+        }
     },
 
     getOneById: async (req, res) => {
