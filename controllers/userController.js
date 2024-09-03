152a5613398695f8481f8990ba4b65bb6ff4b3a7
Josh Stringer <joshstringer@live.com>
2024-09-03 09:58:30 -0500

    delete user works now

--- controllers/userController.js
+++ controllers/userController.js
@@ -73,7 +73,24 @@ const UsersController = {
     },
 
     deleteUser: async (req, res) => {
-        res.send('this is user - delete user');
+        try {
+            const userId = req.params.id;
+
+            if (!userId) {
+                return res.status(400).json({ message: 'User required' });
+            }
+
+            const deletedUser = await User.findByIdAndDelete(userId);
+
+            if (!deletedUser) {
+                return res.status(404).json({ message: 'User not found' });
+            }
+
+            res.status(200).json({ message: 'User deleted', deletedUser });
+        } catch (error) {
+            console.error('Error deleting User:', error);
+            res.status(500).json({ message: 'Error deleting User', error });
+        }
     }
 };
 
